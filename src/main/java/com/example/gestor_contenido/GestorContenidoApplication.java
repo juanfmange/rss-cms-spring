f314c772a28b60e40f2974bef16cf654b7e884ea
juanfmange <juanfmange@gmail.com>
2025-07-01 18:38:19 -0600

    start spring

---
+++ src/main/java/com/example/gestor_contenido/GestorContenidoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestor_contenido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorContenidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorContenidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestor_contenido/GestorContenidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestor_contenido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorContenidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
